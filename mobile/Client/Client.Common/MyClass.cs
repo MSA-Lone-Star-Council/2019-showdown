95d178318705afa7c90eae098f91e6d911477642
Zuhair Parvez <zuhairparvez@gmail.com>
2017-02-25 12:55:00 -0800

    Added new and improved showdown solution

---
+++ mobile/Client/Client.Common/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Client.Common
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
---
+++ mobile/Common/Common.iOS/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Common.iOS
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
