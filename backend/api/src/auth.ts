ad367a51bbbd8ecdced67493b2e330436b7e0091
Zuhair Parvez <zuhairparvez@gmail.com>
2017-02-26 12:24:23 -0800

    Authenticate with JSON Web Tokens

--- backend/api/src/auth.ts
+++ backend/api/src/auth.ts
@@ -1,16 +1,16 @@
 import * as Koa from 'koa';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 import * as winston from 'winston';
 
-class User {
-    username: string;
-    constructor(username: string){
-        this.username = username;
-    }
+interface UserClaims {
+    sub: string;
+    iss: string;
+    permissions: string;
 }
 
 export interface UserState {
-    user?: User;
+    user?: UserClaims;
 }
 
 export class UnauthenticatedUserError implements Error {
@@ -19,42 +19,25 @@ export class UnauthenticatedUserError implements Error {
     stack: string;
 }
 
-/**
- * Extracts the username and password from a Basic Authentication header
- * For example: "Basic YWRtaW46cGEkJHcwcmQ=" returns { username: 'admin', password: 'pa$$w0rd'}
- * @param {string} authorizationHeader The value for the 'authorization' header
- * @returns {object} username and password encoded in 'authorization' header
- */
-function extractUsernameAndPassword(authorizationHeader: string) : { username: string, password: string } {
-    const authorizeHeader: string[] = authorizationHeader.split(' ');
-    const [, base64string] = authorizeHeader;  // Ignores the string 'Basic'
-
-    // base64 is decoded into a string of form username:password
-    const [username, password] = Buffer.from(base64string, 'base64').toString('UTF-8').split(':');
-    return {
-        username,
-        password
-    }
+export class UnauthorizedUserError implements Error {
+    name: string = "UnauthorizedUserError";
+    message: string = "User is unauthorized";
+    stack: string;
 }
 
-/**
- * Create a middleware function for authenticating against a single username and password with Basic Auth
- * @param {string} username The username for logging in
- * @param {string} hashedPassword The encrypted password the user will use
- * @return {Koa.Middleware} Middleware function that reads Basic Auth headers and authenticates users
- */
-export function authenticator(username: string, hashedPassword: string) : Koa.Middleware {
+}
+
+export function authenticator(secret: string) : Koa.Middleware {
     return async function(ctx, next) {
-        if (ctx.header['authorization']) {
-            const { username, password: plaintextPassword } = extractUsernameAndPassword(ctx.header['authorization']);
-            const valid = await bcrypt.compare(plaintextPassword, hashedPassword);
-            if(valid) {
-                winston.info(`User ${ username } authenticated successfully`);
+        const authorizationHeader: string = ctx.header['authorization'];
+        if (authorizationHeader) {
+            const [, token] = authorizationHeader.split(' ');
+            try {
+                const decodedToken = jwt.verify(token, secret);
                 const userState: UserState = ctx.state as UserState;
-                userState.user = new User(username); 
-            }
-            else {
-                winston.warn(`User ${ username } failed to authenticate`);
+                userState.user = decodedToken as UserClaims;
+            } catch (err) {
+                winston.warn(`Invalid JWT token supplied`);
             }
         }
 
@@ -62,10 +45,12 @@ export function authenticator(username: string, hashedPassword: string) : Koa.Mi
             await next();
         }
         catch (e) {
-            if (e instanceof UnauthenticatedUserError){
+            if (e instanceof UnauthenticatedUserError) {
                 ctx.response.status = 401;
-                ctx.set('WWW-Authenticate', 'Basic realm="User Visible Realm"');
+            }
+            else if (e instanceof UnauthorizedUserError) {
+                ctx.response.status = 403;
             }
         }
     }
-}
+} 
