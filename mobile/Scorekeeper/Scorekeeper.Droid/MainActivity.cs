ec9c50985bc9c0265630cf3a9d3ce1e945ff334b
Saad Najmi <saadnajmi2@gmail.com>
2017-03-03 16:29:37 -0600

    Deleted and re-added all the Android Projects. (#19)
    * Deleted and re-added all the Android Projects.
    
    * Moved Common.Droid to the appropriate folder
    
    * Made changes Zuhair requested

---
+++ mobile/Scorekeeper/Scorekeeper.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Scorekeeper.Droid
+{
+    [Activity(Label = "Scorekeeper.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
