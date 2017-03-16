c10b65ad8e7087005ad8391f4bf2021bb88b8989
Saad Najmi <saadnajmi2@gmail.com>
2017-03-16 16:40:27 -0500

    Added methods to post, update, and delete events from the schedule

--- mobile/Common/Common.Common/RestService.cs
+++ mobile/Common/Common.Common/RestService.cs
@@ -14,6 +14,10 @@ namespace Common.Common
     {
         HttpClient client;
 
+        //Move to a "Constants" file once finalized
+        static string RestUrl = "http://developer.xamarin.com:8081/api/todoitems{0}";
+
+
         public RestService()
         {
             client = new HttpClient()
@@ -22,12 +26,10 @@ namespace Common.Common
             };
         }
 
-        public async Task<List<Event>> RefreshScheduleAsync()
+        //Get a list of Schedule Events
+        public async Task<List<Event>> RefreshSchedule()
         {
             var Events = new List<Event>();
-
-            //Move to a "Constants" file once finalized
-            string RestUrl = "http://developer.xamarin.com:8081/api/todoitems{0}"; 
 
             var uri = new Uri(string.Format(RestUrl, string.Empty));
 
@@ -44,9 +46,58 @@ namespace Common.Common
             {
                 Debug.WriteLine(@"				ERROR {0}", ex.Message);
             }
-
             return Events;
         }
 
+        //Add a new Event
+        public async Task AddEvent(Event item, bool isNewItem = false)
+        {
+            var uri = new Uri(string.Format(RestUrl, item.Id));
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(item);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                if (isNewItem)
+                {
+                    response = await client.PostAsync(uri, content);
+                }
+                else
+                {
+                    response = await client.PutAsync(uri, content);
+                }
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"				TodoItem successfully saved.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"				ERROR {0}", ex.Message);
+            }
+        }
+
+        //Delete an event
+        public async Task DeleteEvent(Event item)   //"Event item", or "string id"?
+        {
+            var uri = new Uri(string.Format(RestUrl, item.Id));
+            try
+            {
+                var response = await client.DeleteAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"				TodoItem successfully deleted.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"				ERROR {0}", ex.Message);
+            }
+        }
     }
 }
