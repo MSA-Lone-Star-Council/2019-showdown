aa5fe12da3f9c68bab45a32ac51eef69730e8766
Saad Najmi <saadnajmi2@gmail.com>
2017-03-16 15:35:49 -0500

    Added a REST service and Event Model to Common.Common

---
+++ mobile/Common/Common.Common/Models/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Common.Models
+{
+    class Event
+    {
+        public string Id { get; set; }
+
+        //TODO: Make this a DateTime Object that's compatible cross-platform
+        public string StartTime { get; set; }
+
+        //TODO: Make this a DateTime Object that's compatible cross-platform
+        public string EndTime { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+    }
+}
---
+++ mobile/Common/Common.Common/RestService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Common.Common.Models;
+
+namespace Common.Common
+{
+    class RestService
+    {
+        HttpClient client;
+
+        public RestService()
+        {
+            client = new HttpClient()
+            {
+                MaxResponseContentBufferSize = 256000
+            };
+        }
+
+        public async Task<List<Event>> RefreshScheduleAsync()
+        {
+            var Events = new List<Event>();
+
+            //Move to a "Constants" file once finalized
+            string RestUrl = "http://developer.xamarin.com:8081/api/todoitems{0}"; 
+
+            var uri = new Uri(string.Format(RestUrl, string.Empty));
+
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Events = JsonConvert.DeserializeObject<List<Event>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"				ERROR {0}", ex.Message);
+            }
+
+            return Events;
+        }
+
+    }
+}
