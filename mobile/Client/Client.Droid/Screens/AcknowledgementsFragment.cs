0b80213937344bb8b2df6f0ddcf9f81e67e4d14d
Saad Najmi <saadnajmi2@gmail.com>
2017-03-19 01:23:23 -0500

    Navigation Drawer, working on the emulator

---
+++ mobile/Client/Client.Droid/Screens/AcknowledgementsFragment.cs
@@ -0,0 +1,27 @@
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+
+namespace Client.Droid.Screens
+{
+    public class AcknowledgementsFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.fragment_acknowledgements, container, false);
+
+        }
+    }
+}
