b6582bf9d846f050a02e2dbd12fff24c43091189
Zuhair Parvez <zuhairparvez@gmail.com>
2017-03-18 21:06:56 -0700

    Add structure for iOS client

---
+++ mobile/Client/Client.iOS/Screens/AcknowledgementsViewController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UIKit;
+
+namespace Client.iOS
+{
+	public class AcknowledgementsViewController : UIViewController
+	{
+		public AcknowledgementsViewController()
+		{
+			View.BackgroundColor = UIColor.Blue;
+		}
+	}
+}
