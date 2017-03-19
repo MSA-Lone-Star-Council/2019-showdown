091cba604331efff9a1e4b8365021a91517b4861
Zuhair Parvez <zuhairparvez@gmail.com>
2017-03-18 21:57:34 -0700

    iOS Client Structure (#38)
    * Die Resource files
    
    * Created iOS app structure

---
+++ mobile/Client/Client.iOS/Screens/AnnoucementsViewController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UIKit;
+
+namespace Client.iOS
+{
+	public class AnnoucementsViewController : UIViewController
+	{
+		public AnnoucementsViewController()
+		{
+			View.BackgroundColor = UIColor.Yellow;
+		}
+	}
+}
