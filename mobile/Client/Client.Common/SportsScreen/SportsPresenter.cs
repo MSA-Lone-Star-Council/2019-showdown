112c36cb2d7dc543a7f531e0af873a18f7f495f6
Zuhair Parvez <zuhairparvez@gmail.com>
2017-03-19 16:35:46 -0700

    Implemented a rudimentary sports page (#45)
    * Implemented a rudimentary sports page
    
    * Fix merge conflicts
    
    * Made the sports screen a little prettier

---
+++ mobile/Client/Client.Common/SportsScreen/SportsPresenter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Common;
+using Common.Common.Models;
+
+namespace Client.Common
+{
+	public class SportsPresenter : Presenter<ISportsView>
+	{
+
+		public async Task OnBegin()
+		{
+			await UpdateFromServer();
+		}
+
+		public async Task OnTick()
+		{
+			await UpdateFromServer();
+		}
+
+		public async Task OnClickRow(Game row)
+		{
+			View.OpenGame(row.ID);
+		}
+
+		public async Task OnStar(Game game)
+		{
+			View.ShowMessage("Subscribed for notifications for this game!");
+			// TODO: What the message says
+		}
+
+		private async Task UpdateFromServer()
+		{
+			var gamesFromServer = await GetAllGames();
+
+			if (View != null)
+				View.Games = gamesFromServer.OrderByDescending(g => g.Time).ToList();
+		}
+
+		private async Task<List<Game>> GetAllGames()
+		{
+
+			var baseTime = DateTimeOffset.Now;
+
+
+			// TODO: Hit the backend
+			Event basketball = new Event() { Title = "Brother's Basketball" };
+			Event volleyball = new Event() { Title = "Sister's Vollyeball" };
+
+			Game bballLoserFinals = new Game() { 
+				Title = "Loser Bracket Finals", 
+				Time = baseTime.Subtract(TimeSpan.FromSeconds(30*60 + 12)),
+				Event = basketball
+			};
+			Game bballFinals = new Game() { 
+				Title = "Finals", 
+				Time = baseTime.Subtract(TimeSpan.FromSeconds(45*60 + 28)),
+				Event = basketball
+			};
+
+			Score utAustinBBallScore = new Score() { Team = "UT Austin", Points = 90 };
+			Score utDallasBBallScore = new Score() { Team = "UT Dallas", Points = 10 };
+
+
+			Score uHoustonBBallScore = new Score() { Team = "UH", Points = 40 };
+			Score texasTechBBallScore = new Score() { Team = "Texas Tech", Points = 30 };
+
+			bballFinals.Score = new List<Score>() { utAustinBBallScore, utDallasBBallScore };
+			bballFinals.Teams = new List<string>() { "UT Austin", "UT Dallas" };
+
+			bballLoserFinals.Score = new List<Score> { uHoustonBBallScore, texasTechBBallScore };
+
+			Game volleyballElim = new Game() { 
+				Title = "SMU vs UTA Elimination", 
+				Time = baseTime.Subtract(TimeSpan.FromSeconds(20*60 + 48)),
+				Event = volleyball
+			};
+			Score smuScore = new Score() { Team = "SMU", Points = 25 };
+			Score utaScore = new Score() { Team = "UT Arlington", Points = 10 };
+			volleyballElim.Score = new List<Score> { smuScore, utaScore };
+			volleyballElim.Teams = new List<string>() { "SMU", "UT Arlington" };
+
+
+			return new List<Game>() { bballLoserFinals, bballFinals, volleyballElim };
+		}
+	}
+}
--- mobile/Client/Client.iOS/Screens/SportsViewController.cs
+++ mobile/Client/Client.iOS/Screens/SportsViewController.cs
@@ -1,13 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
+using Client.Common;
+using Common.Common.Models;
+using Foundation;
 using UIKit;
 
 namespace Client.iOS
 {
-	public class SportsViewController : UIViewController
+	public class SportsViewController : UIViewController, ISportsView
 	{
+		static NSString GameCellID = new NSString("GameCellId");
+
+		SportsPresenter Presenter { get; set; }
+
+		UITableView GamesList { get; set; }
+
+
 		public SportsViewController()
 		{
-			View.BackgroundColor = UIColor.Green;
+			Presenter = new SportsPresenter();
+
+			Presenter.TakeView(this);
+		}
+
+		public async override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			View.BackgroundColor = new UIColor(0.90f, 1.0f, 0.91f, 1.0f);
+
+			GamesList = new UITableView(View.Bounds)
+			{
+				BackgroundColor = UIColor.Clear,
+				Source = new SportsTableSource(),
+				RowHeight = 155,
+				SeparatorStyle = UITableViewCellSeparatorStyle.None
+			};
+			GamesList.RegisterClassForCellReuse(typeof(GameCell), GameCellID);
+			View.AddSubview(GamesList);
+
+			await Presenter.OnBegin();
+		}
+
+
+		public override void ViewWillDisappear(bool animated)
+		{
+			base.ViewWillDisappear(animated);
+			Presenter.RemoveView();
+		}
+
+		public List<Game> Games
+		{
+			set
+			{
+				SportsTableSource sts = GamesList.Source as SportsTableSource;
+				sts.Games = value;
+				GamesList.ReloadData();
+			}
+		}
+
+		public void OpenGame(string gameId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ShowMessage(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		class SportsTableSource : UITableViewSource
+		{
+			public List<Game> Games { get; set; }
+
+			public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+			{
+				var cell = tableView.DequeueReusableCell(GameCellID) as GameCell;
+				cell.BackgroundColor = UIColor.Clear;
+
+				var game = Games[indexPath.Row];
+
+				cell.UpdateCell(game);
+
+				return cell;
+			}
+
+			public override nint RowsInSection(UITableView tableview, nint section)
+			{
+				return Games.Count;
+			}
 		}
 	}
 }
