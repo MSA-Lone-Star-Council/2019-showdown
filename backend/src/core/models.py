a29b988efb9f447f53e0dc7c312b628954a4b95d
Zuhair Parvez <zuhairparvez@gmail.com>
2017-03-25 12:14:47 -0700

    Add short name to school model

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=80)
+    short_name = models.CharField(max_length=20)
     slug = models.SlugField()
     logo = models.URLField()
