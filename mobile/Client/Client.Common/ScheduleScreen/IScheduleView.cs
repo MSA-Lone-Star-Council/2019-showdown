a7d67535e8511666b1b839727cf36088bee53021
Saad Najmi <saadnajmi2@gmail.com>
2017-03-24 19:43:11 -0500

    Made Schedule Presenter and IGameView files

---
+++ mobile/Client/Client.Common/ScheduleScreen/IScheduleView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Common.Models;
+
+namespace Client.Common
+{
+    interface IScheduleView
+    {
+        List<Event> events { get; }
+    }
+}
