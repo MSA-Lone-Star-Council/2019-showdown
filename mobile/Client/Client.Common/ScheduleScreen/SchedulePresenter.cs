85cdd971b411510cb1a8ed4ec1fdc8ed41487dab
Saad Najmi <saadnajmi2@gmail.com>
2017-03-25 13:50:55 -0500

    Added most of the SchedulePresenter Code. Some To-do's and loose ends to clean up

--- mobile/Client/Client.Common/ScheduleScreen/SchedulePresenter.cs
+++ mobile/Client/Client.Common/ScheduleScreen/SchedulePresenter.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Common;
+using Common.Common.Models;
+
 
 namespace Client.Common
 {
-    class SchedulePresenter
+    public class SchedulePresenter : Presenter<IScheduleView>
     {
+        public async Task OnBegin()
+        {
+            await UpdateFromServer();
+        }
+
+        public async Task OnTick()
+        {
+            await UpdateFromServer();
+        }
+
+        public async Task OnStar()
+        {
+            View.ShowMessage("Subscribed for notifications for this game!");
+            // TODO: What the message says
+        }
+
+        public void OnClickRow(Event row)
+        {
+            View.OpenEvent(row);
+        }
+
+        private async Task<List<Event>> UpdateFromServer()
+        {
+            return ShowdownRESTClient.MakeFakeData();
+        }
     }
 }
