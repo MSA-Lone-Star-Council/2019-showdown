f9320af377003d334d263ab260d73506e6e69a3f
Zuhair Parvez <zuhairparvez@gmail.com>
2017-03-25 17:15:05 -0700

    Add announcements model

--- backend/src/notifications/models.py
+++ backend/src/notifications/models.py
@@ -1,12 +1,6 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 
-class Topic(models.Model):
-    name = models.CharField(max_length=25, unique=True)
-
-class Message(models.Model):
-    header = models.CharField(max_length=80, blank=False)
-    summary = models.CharField(max_length=80, blank=False)
-    meta = JSONField()  # Dump mobile platform specific stuff in here
-
-    topics = models.ManyToManyField(Topic)
+class Announcement(models.Model):
+    title = models.CharField(max_length=40)
+    body = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
