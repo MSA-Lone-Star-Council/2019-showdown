6fc3218d8a3438eba16f0b5ec29a0a6bb75d4145
Zuhair Parvez <zuhairparvez@gmail.com>
2017-03-30 00:12:54 -0700

    fixed score model quirkiness

--- backend/src/core/serializers.py
+++ backend/src/core/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import serializers
 
 from scores.models import Game
@@ -5,12 +7,19 @@ from scores.serializers import GameSerializer
 
 from .models import School
 
+import logging
+logger = logging.getLogger('showdown.%s' % __name__)
+
+class ShortSchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = ('slug', 'name', 'short_name', 'logo')
 
 class SchoolSerializer(serializers.ModelSerializer):
     games = serializers.SerializerMethodField('get_latest_scores')
 
     def get_latest_scores(self, obj):
-        games = Game.objects.filter(teams=obj)
+        games = Game.objects.filter(Q(home_team=obj) | Q(away_team=obj))
         return GameSerializer(games, many=True).data
     
     class Meta:
--- backend/src/scores/views.py
+++ backend/src/scores/views.py
@@ -3,17 +3,16 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from .models import Game
-from .serializers import GameSerializer, ScoreCardSerializer
+from .serializers import GameSerializer, ScoreSerializer
 
 class GamesView(generics.ListAPIView):
     serializer_class = GameSerializer
     queryset = Game.objects.all()
 
 class ScoresForGameView(generics.ListAPIView):
-    serializer_class = ScoreCardSerializer
+    serializer_class = ScoreSerializer
 
     def get_queryset(self):
         game_id = self.kwargs.get('game_id')
         game = get_object_or_404(Game, pk=game_id)
-
-        return game.scorecard_set.order_by('-time')
+        return game.scores.all()
