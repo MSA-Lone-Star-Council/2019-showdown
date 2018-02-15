b6bc77e83d30dd6fd6b97c68b4b9bae42108c42b
Saad Najmi <saadnajmi2@gmail.com>
2018-02-15 05:37:08 -0600

    Integrate app center (#119)
    * Seems a lot of frameworks got updated.
    Also , "timer.invalidate()" is now "if (timer != null) timer.invalidate()" to prevent crashes wen the backend is not present.
    
    * Added App Center references, and a Secrets.cs file with fake secrets in order for cloud builds to pass
    
    * forgot a file
    
    * Fine, I'll add these files as well
    
    * One more fake file

---
+++ mobile/Common/Common.iOS/Resources/Colors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace Common.iOS.Resources
+{
+    public class Colors
+    {
+        public static UIColor primaryColor      = new UIColor(red: 0.75f, green: 0.34f, blue: 0.00f, alpha: 1.0f);
+        public static UIColor primaryLightColor = new UIColor(red: 0.97f, green: 0.53f, blue: 0.22f, alpha: 1.0f);
+        public static UIColor primaryDarkColor  = new UIColor(red: 0.53f, green: 0.16f, blue: 0.00f, alpha: 1.0f);
+        public static UIColor primaryTextColor  = new UIColor(red: 1.00f, green: 1.00f, blue: 1.00f, alpha: 1.0f);
+    }
+}
