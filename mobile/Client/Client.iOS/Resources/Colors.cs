980e132bf564c4d54bc610071638243d400dea24
Saad Najmi <saadnajmi2@gmail.com>
2018-02-26 08:51:35 -0600

    Added burnt orange colors for showdown

---
+++ mobile/Client/Client.iOS/Resources/Colors.cs
@@ -0,0 +1,16 @@
+﻿using UIKit;
+
+namespace Client.iOS.Resources
+{
+    class Colors
+    {
+        UIColor primaryColor = new UIColor(red: 0.75f, green: 0.34f, blue: 0.00f, alpha: 1.0f);
+        UIColor primaryLightColor = new UIColor(red: 0.97f, green: 0.53f, blue: 0.22f, alpha: 1.0f);
+        UIColor primaryDarkColor = new UIColor(red: 0.53f, green: 0.16f, blue: 0.00f, alpha: 1.0f);
+        UIColor primaryTextColor = new UIColor(red: 1.00f, green: 1.00f, blue: 1.00f, alpha: 1.0f);
+        UIColor secondaryColor = new UIColor(red: 0.20f, green: 0.25f, blue: 0.28f, alpha: 1.0f);
+        UIColor secondaryLightColor = new UIColor(red: 0.36f, green: 0.42f, blue: 0.45f, alpha: 1.0f);
+        UIColor secondaryDarkColor = new UIColor(red: 0.05f, green: 0.10f, blue: 0.13f, alpha: 1.0f);
+        UIColor secondaryTextColor = new UIColor(red: 1.00f, green: 1.00f, blue: 1.00f, alpha: 1.0f);
+    }
+}
