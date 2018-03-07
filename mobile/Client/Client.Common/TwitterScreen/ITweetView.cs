f6c1d684310d718f62430bfedb0d4efaae7cc77c
Saad Najmi <saadnajmi2@gmail.com>
2018-03-07 13:03:59 -0600

    Moved EventHandler to ITweetView interface

--- mobile/Client/Client.Common/TwitterScreen/ITweetView.cs
+++ mobile/Client/Client.Common/TwitterScreen/ITweetView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tweetinvi.Events;
 using Tweetinvi.Models;
 
 namespace Client.Common
@@ -11,8 +12,9 @@ namespace Client.Common
     {
         List<ITweet> Tweets { get; set; }
 
-        void OpenTweet(ITweet tweet);
-
         void Refresh();
+
+        void OpenTweet(ITweet tweet);
+        EventHandler<MatchedTweetReceivedEventArgs> AddTweetFromStream();
     }
 }
--- mobile/Client/Client.Common/TwitterScreen/TweetPresenter.cs
+++ mobile/Client/Client.Common/TwitterScreen/TweetPresenter.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Common.Common;
 using Tweetinvi;
-using Tweetinvi.Events;
 using Tweetinvi.Models;
 using Tweetinvi.Streaming;
 
@@ -17,37 +14,25 @@ namespace Client.Common
         private IFilteredStream filteredStream;
         private TwitterCredentials twitterCredentials;
         private readonly ShowdownRESTClient _client;
-        private readonly IClientUi _clientUi;
         private static readonly string TRACK_HASHTAG = "#blackpanther";
 
         List<ITweet> tweets;
 
-        public TweetPresenter(ShowdownRESTClient client, IClientUi clientUi)
+        public TweetPresenter(ShowdownRESTClient client)
         {
             _client = client;
-            _clientUi = clientUi;
             filteredStream = Stream.CreateFilteredStream();
             SetTwitterCredentials();
         }
 
         public async Task OnBegin()
         {
-            filteredStream.AddTrack(TRACK_HASHTAG);
             await GetCachedTweets();
             filteredStream.AddTrack(TRACK_HASHTAG);
-            filteredStream.MatchingTweetReceived += (sender, arg) =>
-            {
-                _clientUi.updateClientUi();
-            };
+            filteredStream.MatchingTweetReceived += View.AddTweetFromStream();
             filteredStream.StartStreamMatchingAnyCondition();
-
         }
 
-        //Something like this, where we specify this event handler is to be implemented by  the platform specific classes
-        public EventHandler<MatchedTweetReceivedEventArgs> AddTweetInRealtime()
-        {
-            throw new NotImplementedException();
-        }
 
         public void OnClickRow(ITweet Tweet)
         {
