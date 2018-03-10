ca2e511f7a871aa16ceefc6f8ff9d6606770764f
Saad Najmi <saadnajmi2@gmail.com>
2018-03-10 16:51:00 -0600

    Client twitter common (#134)
    * Create Twitter client structure
    
    * Added tweetinvi to client.common
    
    * Completed Twitter backend library and TweetView
    
    * Updated namespaces and added TweetInvi to Client.Droid and Client.iOS
    
    * Updated the Tweet Presenter
    
    * Refactored to have a separate GetCachedTweets and an EventHandler for realtime tweets. Will need to check on style if this is the best way to go
    
    * Updated TweetPresenter to incorporate TweetLibrary. Added interface to use to update UI on a given platform
    
    * Moved auth to constructor and used interface for updating UI in OnBegin()
    
    * Moved EventHandler to ITweetView interface
    
    * This needs to be deleted anyway

---
+++ mobile/Client/Client.Common/TwitterScreen/ITweetView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi.Events;
+using Tweetinvi.Models;
+
+namespace Client.Common
+{
+    public interface ITweetView
+    {
+        List<ITweet> Tweets { get; set; }
+
+        void Refresh();
+
+        void OpenTweet(ITweet tweet);
+        EventHandler<MatchedTweetReceivedEventArgs> AddTweetFromStream();
+    }
+}
---
+++ mobile/Client/Client.Common/TwitterScreen/TweetPresenter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common.Common;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Streaming;
+
+namespace Client.Common
+{
+    public class TweetPresenter : Presenter<ITweetView>
+    {
+
+        private IFilteredStream filteredStream;
+        private TwitterCredentials twitterCredentials;
+        private readonly ShowdownRESTClient _client;
+        private static readonly string TRACK_HASHTAG = "#blackpanther";
+
+        List<ITweet> tweets;
+
+        public TweetPresenter(ShowdownRESTClient client)
+        {
+            _client = client;
+            filteredStream = Stream.CreateFilteredStream();
+            SetTwitterCredentials();
+        }
+
+        public async Task OnBegin()
+        {
+            await GetCachedTweets();
+            filteredStream.AddTrack(TRACK_HASHTAG);
+            filteredStream.MatchingTweetReceived += View.AddTweetFromStream();
+            filteredStream.StartStreamMatchingAnyCondition();
+        }
+
+
+        public void OnClickRow(ITweet Tweet)
+        {
+            View.OpenTweet(Tweet);
+        }
+
+        private Task UpdateFromServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task GetCachedTweets()
+        {
+            //TODO Make a local model of Itweet
+            //TODO Update Backend so it actually does this method
+            /*
+            tweets = await _client.GetTweetsAsync();    
+            if (View != null) View.Tweets = tweets;
+            */
+            await Task.CompletedTask;
+        }
+
+        private void SetTwitterCredentials()
+        {
+            twitterCredentials = new TwitterCredentials(
+            Secrets.twitterConsumerKey,
+            Secrets.twitterConsumerSecret,
+            Secrets.twitterAccessToken,
+            Secrets.twitterAccessTokenSecret)
+            {
+                ApplicationOnlyBearerToken = Secrets.twitterBearerToken
+            };
+            Auth.SetCredentials(twitterCredentials);
+        }
+    }
+}
