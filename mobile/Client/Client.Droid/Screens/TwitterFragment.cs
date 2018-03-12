2d9f90efc73c381d11dd20683e90ff7da5d569e8
Ali Naqvi <alinaqvi1397@gmail.com>
2018-03-11 22:45:31 -0500

    Formatting fixes and stuff

--- mobile/Client/Client.Droid/Screens/TwitterFragment.cs
+++ mobile/Client/Client.Droid/Screens/TwitterFragment.cs
@@ -1,57 +1,55 @@
-﻿using System;
-using Android.Support.V4.App;
+﻿using Android.Support.V4.App;
 using Android.OS;
 using Android.Views;
 using Client.Common;
 using Android.Content;
 using Android.Net;
 using Android.Widget;
-using Android.Support.CustomTabs;
 using Android.Webkit;
 
 namespace Client.Droid
 {
-    public class TwitterFragment : Fragment, ITweetView
-    {
-        TweetPresenter Presenter { get; set; }
-		
+	public class TwitterFragment : Fragment, ITweetView
+	{
+		TweetPresenter Presenter { get; set; }
+
 		private TextView emptyTextView;
 		private ProgressBar progressBar;
 		private WebView webView;
 
 		public override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+		{
+			base.OnCreate(savedInstanceState);
 
-            Presenter = new TweetPresenter();
-            Presenter.TakeView(this);
+			Presenter = new TweetPresenter();
+			Presenter.TakeView(this);
 		}
 
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            View view = inflater.Inflate(Resource.Layout.fragment_twitter, container, false);
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			View view = inflater.Inflate(Resource.Layout.fragment_twitter, container, false);
 			emptyTextView = view.FindViewById<TextView>(Resource.Id.empty_view);
 			progressBar = view.FindViewById<ProgressBar>(Resource.Id.progress_spinner);
 			webView = view.FindViewById<WebView>(Resource.Id.web_view);
 			Presenter.OnBegin();
 			return view;
-        }
+		}
 
-        public override void OnResume()
-        {
-            base.OnResume();
-            Presenter.TakeView(this);
+		public override void OnResume()
+		{
+			base.OnResume();
+			Presenter.TakeView(this);
 		}
 
 		public override void OnStop()
-        {
-            base.OnStop();
-            Presenter.RemoveView();
-        }
+		{
+			base.OnStop();
+			Presenter.RemoveView();
+		}
 
 		bool ITweetView.HasInternetConnection()
 		{
-			ConnectivityManager connectivityManager = (ConnectivityManager) Context.GetSystemService(Context.ConnectivityService);
+			ConnectivityManager connectivityManager = (ConnectivityManager)Context.GetSystemService(Context.ConnectivityService);
 			NetworkInfo networkInfo = connectivityManager.ActiveNetworkInfo;
 			bool isConnected = networkInfo != null && networkInfo.IsConnectedOrConnecting;
 			return isConnected;
