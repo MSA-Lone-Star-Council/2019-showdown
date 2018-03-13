f791b478de6c4fce02c7f1a211139721066ad285
Saad Najmi <saadnajmi2@gmail.com>
2018-03-13 14:48:26 -0500

    Twitter works but isn't scaled properly

--- mobile/Client/Client.iOS/Screens/TwitterViewController.cs
+++ mobile/Client/Client.iOS/Screens/TwitterViewController.cs
@@ -1,10 +1,55 @@
 ﻿using System;
-namespace Client.iOS.Screens
+using Client.Common;
+using Foundation;
+using UIKit;
+
+namespace Client.iOS
 {
-    public class TwitterViewController
+    public class TwitterViewController : UIViewController, ITweetView
     {
-        public TwitterViewController()
+        TweetPresenter presenter;
+        UIWebView webView;
+
+        public TwitterViewController() : base("TwitterViewController", null)
         {
+            presenter = new TweetPresenter();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            webView = new UIWebView(View.Bounds);
+            webView.ScalesPageToFit = true;
+            View.AddSubview(webView);
+
+            presenter.TakeView(this);
+            presenter.OnBegin();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        void ITweetView.StartWebView(string url)
+        {
+            //throw new NotImplementedException();
+            webView.LoadRequest(new NSUrlRequest(new NSUrl(url)));
+        }
+
+        void ITweetView.NoInternetConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ITweetView.HasInternetConnection()
+        {
+            //throw new NotImplementedException();
+            return true;
         }
     }
 }
+
