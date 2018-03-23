8f09290fc8f9cc236a00f32a5c43bdd3dd95192f
Saad Najmi <saadnajmi2@gmail.com>
2018-03-22 21:10:56 -0500

    Moved Connectivity to a new file

---
+++ mobile/Common/Common.Common/Connectivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Common.Common
+{
+    public static class Connectivity
+    {
+        public static bool IsConnected()
+        {
+            return CrossConnectivity.Current.IsConnected;
+        }
+
+        public static async Task<bool> IsBackendReachable()
+        {
+            var connectivity = CrossConnectivity.Current;
+            if (!connectivity.IsConnected) return false;
+
+            var reachable = await connectivity.IsRemoteReachable(Secrets.BACKEND_URL);
+
+            return reachable;
+        }
+
+    }
+}
